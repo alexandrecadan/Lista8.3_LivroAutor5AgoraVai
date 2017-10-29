6260f16104e878836e96c187b39ef664d0690ba3
alexandrecadan <alexandrecadan@gmail.com>
2017-10-29 12:59:04 -0200

    Switch 5 e 6 ainda não implementados

---
+++ src/lista8/pkg3_livroautor5agoravai/Livro.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista8.pkg3_livroautor5agoravai;
+
+/**
+ *
+ * @author alexandre
+ */
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public final class Livro {
+
+    private int id;
+    private String titulo;
+    private List<Autor> autores;
+
+    public Livro(String titulo,List<Autor> autores) {
+        this.titulo = titulo;
+        this.autores = new ArrayList();
+        this.setAutores(autores);
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        Iterator iterator = autores.iterator();
+        while (iterator.hasNext()) {
+            Autor autor = (Autor) iterator.next();
+            this.adicionarAutor(autor);
+        }
+    }
+
+    public List<Autor> getAutores() {
+        return this.autores;
+    }
+
+    public void adicionarAutor(Autor autor) {
+        if (!this.getAutores().contains(autor)) {
+            this.autores.add(autor);
+            autor.adicionarLivro(this);
+        }
+    }
+
+    public void removerAutor(Autor autor) {
+        if (!this.getAutores().contains(autor)) {
+            this.autores.remove(autor);
+            autor.removerLivro(this);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
+
